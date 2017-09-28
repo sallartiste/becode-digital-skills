8c565b96d623f876863f5bd956ea85af28f71996
sallartiste <sallartiste@gmail.com>
2017-09-28 21:42:09 +0200

    comit du soir

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Becode | Digital Skills</title>
+  <link rel="stylesheet" type="text/css" title="Cool stylesheet" href="style.css">
+</head>
+<body>
+  
+  <script src="main.js" type="text/javascript"></script>
+</body>
+</html>
